3b0665cd8aeb9a1a35f4a7bb01a36d0eda6905d0
Nik Rykov <rykov.nikas@gmail.com>
2022-11-18 22:14:48 +0000

    Fix working as root nameserver

--- index.js
+++ index.js
@@ -67,18 +67,44 @@ global.config = JSON5.parse(fs.readFileSync("./config.json5", "utf8"));
       edns: true,
       dnssec: true
     })
-
+    let nsLookUpRes = {};
     try {
-      let domainNameservers = (await Promise.all(nsServers.map(async serverName => {
-        return lookupResponse.additional.find(r => r.name == serverName) ? [lookupResponse.additional.find(r => r.name == serverName).data.address] : (await recursiveresolver.lookup(serverName, "A")).answer.map(ans => ans.data.address)
-      }))).reduce((pv, cv) => [...pv, ...cv], []).filter(ns => ns)
-      let nsLookUpRes
+      let rootManagerTxId = nsServers.filter(n => n).map(record => { return config.zones.some(zone => record.endsWith(zone)) ? record.slice(0, -config.zones.find(zone => record.endsWith(zone)).length) : null }).find(a => a)
+      if (!rootManagerTxId) {
+        let domainNameservers = (await Promise.all(nsServers.map(async serverName => {
+          return lookupResponse.additional.find(r => r.name == serverName) ? [lookupResponse.additional.find(r => r.name == serverName).data.address] : (await recursiveresolver.lookup(serverName, "A")).answer.map(ans => ans.data.address)
+        }))).reduce((pv, cv) => [...pv, ...cv], []).filter(ns => ns)
 
-      subresolver.setServers(domainNameservers.filter(n => n))
 
-      nsLookUpRes = await subresolver.lookup(question.name.toLowerCase(), "NS")
 
-      let domainNameserverRecords = (nsLookUpRes).authority.filter(record => record && record.data && record.data.ns).map(record => record.data.ns)
+        subresolver.setServers(domainNameservers.filter(n => n))
+
+        nsLookUpRes = await subresolver.lookup(question.name.toLowerCase(), "NS")
+      } else {
+        rootManagerTxId = arweave.utils.bufferTob64Url(hexToBuffer(base36ToBigInt(rootManagerTxId).toString(16)))
+        let rootManagerTx = await fetch(`http://${config.arweaveGateway}/${rootManagerTxId}`).then(res => res.json())
+        if (!rootManagerTx || typeof rootManagerTx !== "object" || !Array.isArray(rootManagerTx.managers)) {
+          throw new Error("No managers found in tx")
+        }
+        let rootLastZonesTxSearch = await ardb.search("transactions").tags([{ name: "Target-NS-TxID", values: [rootManagerTxId] }]).from(rootManagerTx.managers).sort("HEIGHT_DESC").limit(1).exclude(["anchor"]).find()
+
+        let rootLastZonesTx = rootLastZonesTxSearch.length > 0 ? rootLastZonesTxSearch[0].id : rootManagerTx.recordsTx;
+        if (!rootLastZonesTx) {
+          throw new Error("No records tx found")
+        }
+        let rootZoneData = await fetch(`http://${config.arweaveGateway}/${rootLastZonesTx}`).then(res => res.text());
+        nsLookUpRes = {
+          authority: [],
+          answer: wire.fromZone(rootZoneData, question.name).filter(rec => rec.name == question.name)
+        };
+      }
+      if (nsLookUpRes.authority.length == 0) {
+        res.answer = nsLookUpRes.answer
+        res.authority = []
+        res.send()
+        return
+      }
+      let domainNameserverRecords = (nsLookUpRes).authority.filter(record => record && record.data && record.data.ns).map(record => record.data.ns) || (nsLookUpRes).answer.filter(record => record && record.data && record.data.ns).map(record => record.data.ns)
       let managerTxId = domainNameserverRecords.map(record => { return config.zones.some(zone => record.endsWith(zone)) ? record.slice(0, -config.zones.find(zone => record.endsWith(zone)).length) : null }).find(a => a)
       if (!managerTxId) {
         let generalLookup = await subresolver.lookup(question.name.toLowerCase(), question.type)
@@ -109,7 +135,6 @@ global.config = JSON5.parse(fs.readFileSync("./config.json5", "utf8"));
       res.send()
     }
   });
-
   server.open(53, '0.0.0.0');
 })()
 const hexToBuffer = (hexString) =>
